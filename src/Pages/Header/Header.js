e37a9a1325df5d07c2d1534880b2e7d508cc9642
tonny009 <tabassum.islam009@gmail.com>
2022-11-10 12:59:38 +0900

    Header modified

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -36,7 +36,7 @@ const Header = () => {
     </>
 
     return (
-        <div className="navbar  bg-clr mb-3 ">
+        <div className="navbar  bg-clr mb-2 mt-3 rounded-lg ">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
